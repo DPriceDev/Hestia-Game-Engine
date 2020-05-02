0bb8cbc072b4a48db8e347690dda2b42f44f98a7
DPriceDev <dpricedev@gmail.com>
2020-05-02 09:48:58 +0100

    added resource loader and replaced shader

--- include/util/Logger.hpp
+++ include/util/Logger.hpp
@@ -4,13 +4,21 @@
 #include <iostream>
 #include <string>
 
-/**
- * Log a debug message to console
- * TODO: Add array of items that will follow?
- * TODO: Add to separate thread
- */
-static void LogDebug(std::string tag, std::string msg) {
-    std::cout << "DEBUG: " << tag << " - " << msg << std::endl;
-}
+namespace HGE {
 
+    // todo: Update to singleton
+
+    /**
+     * Log a debug message to console
+     * TODO: Add array of items that will follow?
+     * TODO: Add to separate thread
+     */
+    static void LogDebug(std::string tag, std::string msg) {
+        std::cout << "DEBUG: " << tag << " - " << msg << "\n";
+    }
+
+    static void LogError(std::string tag, std::string msg) {
+        std::cout << "ERROR: " << tag << " - " << msg << "\n";
+    }
+}
 #endif
--- include/util/ResourceLoader.hpp
+++ include/util/ResourceLoader.hpp
@@ -1,11 +1,32 @@
 #ifndef HESTIA_UTIL_RESOURCE_LOADER_HPP_
 #define HESTIA_UTIL_RESOURCE_LOADER_HPP_
 
+#include <string>
+#include <fstream>
+#include <sstream>
 
-class ResourceLoader {
+#include "Logger.hpp"
 
+namespace HGE {
 
-    
-};
+    static void loadFileIntoString(std::string &stringOut, const char * filename) {
+        std::ifstream file;
+        std::stringstream ss;
+
+        file.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+
+        try {
+        file.open(filename);
+
+        ss << file.rdbuf();
+        file.close();
+        
+        stringOut = ss.str();
+        } catch (std::ifstream::failure e) {
+            LogError("Resource Loader", "Failed to load file.");
+            stringOut = "";
+        }
+    }
+}
 
 #endif
