1a1fb18c7e23ac2148e7a09373746fb3268b57d4
DPriceDev <dpricedev@gmail.com>
2020-05-28 11:28:09 +0100

    Added unit tests for the maths classes. added catch2 test cmake and runner class

--- include/maths/maths.h
+++ include/maths/maths.h
@@ -9,20 +9,21 @@
 
 namespace HGE {
 
-    template<typename T>
-    static T randomNumberBetween(T minimum, T maximum) {
+    template<class T>
+    static auto randomNumberBetween(const T minimum, const T maximum) -> T {
+        //todo: static_assert((minimum < maximum), "Maximum is less than minimum.");
         std::mt19937 mt{std::random_device{}()};
         std::uniform_int_distribution<T> dist(minimum, maximum);
         return dist(mt);
     }
 
-    template<typename T>
-    static T roundValueToMultipleOf(T value, T multiple) {
+    template<class T>
+    constexpr static auto roundValueToMultipleOf(const T value, const T multiple) -> T {
         return floor(((value + multiple - 1) / multiple)) * multiple;
     }
 
-    template<typename V, typename T>
-    static T ceilToType(V value) {
+    template<class T, class V>
+    constexpr static auto ceilToType(V value) -> T {
         return static_cast<T>(ceil(value));
     }
 
@@ -32,9 +33,9 @@ namespace HGE {
                <= radius;
     }
 
-    static Vector2f midpointBetween(const Vector2f &a, const Vector2f &b) {
-        return {(a.x + b.x) * 0.5f, (a.y + b.y) * 0.5f};
-    }
+//    static Vector2f midpointBetween(const Vector2f &a, const Vector2f &b) {
+//        return {(a.x + b.x) * 0.5f, (a.y + b.y) * 0.5f};
+//    }
 }// namespace HGE
 
 
---
+++ unit_tests/catch_tests_runner.cpp
@@ -0,0 +1,5 @@
+//
+// Created by David Price on 27/05/2020.
+//
+#define CATCH_CONFIG_MAIN
+#include "include/catch.hpp"
